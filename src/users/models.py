1c4dfc580492c1179765432db8c03d64f7863c5c
AndreiVed <catblues87@gmail.com>
2025-08-27 15:25:05 +0300

    created and migrate Profile model

--- src/users/models.py
+++ src/users/models.py
@@ -1,7 +1,8 @@
 import enum
 from datetime import datetime
+from typing import Optional
 
-from sqlalchemy import String, Integer, Boolean, ForeignKey, DateTime, func, Enum
+from sqlalchemy import String, Integer, Boolean, ForeignKey, DateTime, func, Enum, Date, Text
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from src.db.database import Base
@@ -11,6 +12,11 @@ class GroupEnum(enum.Enum):
     USER = "user"
     MODERATOR = "moderator"
     ADMIN = "admin"
+
+
+class GenderEnum(enum.Enum):
+    MAN = "man"
+    WOMAN = "woman"
 
 
 class Group(Base):
@@ -27,11 +33,36 @@ class User(Base):
     email: Mapped[str] = mapped_column(String(255), unique=True, nullable=False, index=True)
     hashes_password: Mapped[str] = mapped_column("hashed_password", String(255), nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean, default=False, nullable=False)
-    created_at: Mapped[datetime] = mapped_column(DateTime(timezone=True), server_default=func.now(), nullable=False)
+    created_at: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True), server_default=func.now(), nullable=False
+    )
     updated_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now(), nullable=False
     )
     group_id: Mapped[int] = mapped_column(ForeignKey("group.id"))
-    group: Mapped["Group"] = relationship(back_populates="group")
+    group: Mapped["Group"] = relationship("Group", back_populates="group")
+
+    profile: Mapped["Profile"] = relationship("Profile", back_populates="user")
+
+    def create
+
+class Profile(Base):
+    __tablename__ = "profiles"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    first_name: Mapped[Optional[str]] = mapped_column(String(100))
+    last_name: Mapped[Optional[str]] = mapped_column(String(100))
+    avatar: Mapped[Optional[str]] = mapped_column(String(255))
+    gender: Mapped[Optional[GenderEnum]] = mapped_column(Enum(GenderEnum))
+    date_of_birth: Mapped[Optional[Date]] = mapped_column(Date)
+    info: Mapped[Optional[str]] = mapped_column(Text)
+
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id", ondelete="CASCADE"),
+        unique=True,
+        nullable=False
+    )
+    user: Mapped[User] = relationship("User", back_populates="profile")
+
 
 
